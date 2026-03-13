e685386a2eb5c579ba8a768d339794c95f119a91
MayooriEkanthan <mayooriekanthan90@gmail.com>
2026-03-13 12:19:26 +0530

    initial commit

---
+++ src/components/layout/Topbar.jsx
@@ -0,0 +1,15 @@
+function Topbar() {
+  return (
+    <div style={{
+      height: "60px",
+      background: "#f5f5f5",
+      display: "flex",
+      alignItems: "center",
+      paddingLeft: "20px"
+    }}>
+      <h3>RideSync Admin Dashboard</h3>
+    </div>
+  );
+}
+
+export default Topbar;
